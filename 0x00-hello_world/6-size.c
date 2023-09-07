97dc49a429843ec6a0178d9ac47f4dbe7813fe9e
Bhenix <bhenix10@gmail.com>
2023-09-07 13:25:21 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	sizeof(char);
+	sizeof(int);
+	sizeof(float);
+	sizeof(double);
+	sizeof(long);
+	sizeof(long long);
+	sizeof(short);
+
