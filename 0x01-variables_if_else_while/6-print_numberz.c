460dd6c74c6ebb58ef0638fcc078bbee1c2ba98a
Bhenix <bhenix10@gmail.com>
2023-09-08 19:22:27 -0700

    using putchar to print int

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,9 +6,13 @@
  */
 int main(void)
 {
-	for (int i = '0'; i <= '9'; i++)
+	int i = 0;
+       
+	while (i <= 9) 
 	{
-		putchar (i);
+		putchar ('0' + i);
+		i++;
 	}
+	putchar("\n");
 	return (0);
 }
