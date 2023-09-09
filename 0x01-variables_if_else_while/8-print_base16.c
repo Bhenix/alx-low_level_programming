a753ddf1aecb8048042b69c9eb52bb3f88adc833
Bhenix <bhenix10@gmail.com>
2023-09-08 22:49:59 -0700

    base sixteen

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x = '0';
+
+	char y = 'a';
+
+	while (x <= '9')
+	{
+		putchar (x);
+		x++;
+	}
+	while (y <= 'f')
+	{
+		putchar (y);
+		y++;
+	}
+	putchar ('\n');
+	return (0);
+}
